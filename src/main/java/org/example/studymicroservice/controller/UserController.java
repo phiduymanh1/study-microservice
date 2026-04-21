320da2fba392d40521a51710cd914984dc9d585b
PHI_MANH\manhp <phiduymanh.dev@gmail.com>
2026-04-22 06:51:49 +0700

    update

--- src/main/java/org/example/studymicroservice/controller/UserController.java
+++ src/main/java/org/example/studymicroservice/controller/UserController.java
@@ -3,9 +3,11 @@ package org.example.studymicroservice.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(UserController.PATH)
+@RestController
+@RequestMapping(UserController.PATH)
 public class UserController {
 
     public static final String PATH = "/user";
