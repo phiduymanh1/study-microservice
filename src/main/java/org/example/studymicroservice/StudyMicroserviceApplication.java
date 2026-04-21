bc4eeb5f9754394c91e074e751bfae732d3e9cec
PHI_MANH\manhp <phiduymanh.dev@gmail.com>
2026-04-22 05:27:14 +0700

    init commit

---
+++ src/main/java/org/example/studymicroservice/StudyMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.studymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/studymicroservice/StudyMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.studymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
