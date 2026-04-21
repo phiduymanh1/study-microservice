33bf60aa85c17c7a62fc1143d6a45c8c11344558
PHI_MANH\manhp <phiduymanh.dev@gmail.com>
2026-04-22 05:46:51 +0700

    user api

---
+++ src/main/java/org/example/studymicroservice/controller/UserController.java
@@ -0,0 +1,17 @@
+package org.example.studymicroservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(UserController.PATH)
+public class UserController {
+
+    public static final String PATH = "/user";
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Integer> getUser(@PathVariable int id) {
+        return ResponseEntity.ok(id);
+    }
+}
